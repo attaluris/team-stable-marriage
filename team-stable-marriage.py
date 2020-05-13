03f5673058a92c404061cf0064c835d8c146e2a0
Sritej Attaluri <sritej@gmail.com>
2020-05-13 04:42:59 -0700

    refactored and anonymized sample data

---
+++ team-stable-marriage.py
@@ -0,0 +1,153 @@
+# Playground: https://trinket.io/python/f7ad7f9864
+
+ 'companyA':  ['consultantA', 'consultantB', 'consultantC', 'consultantD', 'consultantE', 'consultantF', 'consultantG', 'consultantH'],
+ 'companyB':  ['consultantA', 'consultantC', 'consultantE', 'consultantD', 'consultantB', 'consultantG', 'consultantF', 'consultantH'],
+ 'companyC':  ['consultantA', 'consultantB', 'consultantE', 'consultantD', 'consultantC', 'consultantG', 'consultantH', 'consultantF'],
+ 'companyD':  ['consultantF', 'consultantE', 'consultantD', 'consultantA', 'consultantB', 'consultantC', 'consultantH', 'consultantG'],
+ 'companyA2':  ['consultantA', 'consultantB', 'consultantC', 'consultantD', 'consultantE', 'consultantF', 'consultantG', 'consultantH'],
+ 'companyB2':  ['consultantA', 'consultantC', 'consultantE', 'consultantD', 'consultantB', 'consultantG', 'consultantF', 'consultantH'],
+ 'companyC2':  ['consultantA', 'consultantB', 'consultantE', 'consultantD', 'consultantC', 'consultantG', 'consultantH', 'consultantF'],
+ 'companyD2':  ['consultantF', 'consultantE', 'consultantD', 'consultantA', 'consultantB', 'consultantC', 'consultantH', 'consultantG']
+
+ 'companyA':  ['newbieA', 'newbieB', 'newbieC', 'newbieD', 'newbieE', 'newbieF', 'newbieG', 'newbieH', 'newbieI', 'newbieJ'],
+ 'companyB':  ['newbieB', 'newbieE', 'newbieD', 'newbieF', 'newbieC', 'newbieH', 'newbieJ', 'newbieG', 'newbieI', 'newbieA'],
+ 'companyC':  ['newbieE', 'newbieA', 'newbieB', 'newbieH', 'newbieF', 'newbieD', 'newbieC', 'newbieJ', 'newbieG', 'newbieI'],
+ 'companyD':  ['newbieE', 'newbieB', 'newbieG', 'newbieC', 'newbieI', 'newbieF', 'newbieA', 'newbieH', 'newbieJ', 'newbieD'],
+ 'companyA2':  ['newbieA', 'newbieB', 'newbieC', 'newbieD', 'newbieE', 'newbieF', 'newbieG', 'newbieH', 'newbieI', 'newbieJ'],
+ 'companyB2':  ['newbieB', 'newbieE', 'newbieD', 'newbieF', 'newbieC', 'newbieH', 'newbieJ', 'newbieG', 'newbieI', 'newbieA'],
+ 'companyC2':  ['newbieE', 'newbieA', 'newbieB', 'newbieH', 'newbieF', 'newbieD', 'newbieC', 'newbieJ', 'newbieG', 'newbieI'],
+ 'companyD2':  ['newbieE', 'newbieB', 'newbieG', 'newbieC', 'newbieI', 'newbieF', 'newbieA', 'newbieH', 'newbieJ', 'newbieD'],
+ 'companyA3':  ['newbieA', 'newbieB', 'newbieC', 'newbieD', 'newbieE', 'newbieF', 'newbieG', 'newbieH', 'newbieI', 'newbieJ'],
+ 'companyB3':  ['newbieB', 'newbieE', 'newbieD', 'newbieF', 'newbieC', 'newbieH', 'newbieJ', 'newbieG', 'newbieI', 'newbieA']
+
+ 'consultantE':  ['companyD', 'companyD2', 'companyC', 'companyC2', 'companyA', 'companyA2', 'companyB', 'companyB2'],
+ 'consultantH':  ['companyB', 'companyB2', 'companyC', 'companyC2', 'companyA', 'companyA2', 'companyD', 'companyD2'],
+ 'consultantF':  ['companyD', 'companyD2', 'companyA', 'companyA2', 'companyB', 'companyB2', 'companyC', 'companyC2'],
+ 'consultantA':  ['companyD', 'companyD2', 'companyB', 'companyB2', 'companyA', 'companyA2', 'companyC', 'companyC2'],
+ 'consultantG':  ['companyD', 'companyD2', 'companyC', 'companyC2', 'companyA', 'companyA2', 'companyB', 'companyB2'],
+ 'consultantD':  ['companyD', 'companyD2', 'companyB', 'companyB2', 'companyA', 'companyA2', 'companyC', 'companyC2'],
+ 'consultantC':  ['companyD', 'companyD2', 'companyA', 'companyA2', 'companyB', 'companyB2', 'companyC', 'companyC2'],
+ 'consultantB':  ['companyD', 'companyD2', 'companyA', 'companyA2', 'companyC', 'companyC2', 'companyB', 'companyB2']
+
+ 'newbieA':  ['companyA', 'companyA2', 'companyA3', 'companyD', 'companyD2', 'companyC', 'companyC2', 'companyB', 'companyB2', 'companyB3'],
+ 'newbieB':  ['companyC', 'companyC2', 'companyD', 'companyD2', 'companyA', 'companyA2', 'companyA3', 'companyB', 'companyB2', 'companyB3'],
+ 'newbieC':  ['companyC', 'companyC2', 'companyA', 'companyA2', 'companyA3', 'companyB', 'companyB2', 'companyB3', 'companyD', 'companyD2'],
+ 'newbieD':  ['companyA', 'companyA2', 'companyA3', 'companyC', 'companyC2', 'companyB', 'companyB2', 'companyB3', 'companyD', 'companyD2'],
+ 'newbieE':  ['companyC', 'companyC2', 'companyB', 'companyB2', 'companyB3', 'companyD', 'companyD2', 'companyA', 'companyA2', 'companyA3'],
+ 'newbieF':  ['companyA', 'companyA2', 'companyA3', 'companyD', 'companyD2', 'companyC', 'companyC2', 'companyB', 'companyB2', 'companyB3'],
+ 'newbieG':  ['companyB', 'companyB2', 'companyB3', 'companyC', 'companyC2’, 'companyD', 'companyD2', 'companyA', 'companyA2', 'companyA3'],
+ 'newbieH':  ['companyD', 'companyD2', 'companyB', 'companyB2', 'companyB3', 'companyC', 'companyC2', 'companyA', 'companyA2', 'companyA3'],
+ 'newbieI':  ['companyA', 'companyA2', 'companyA3', 'companyD', 'companyD2', 'companyC', 'companyC2', 'companyB', 'companyB2', 'companyB3'],
+ 'newbieJ':  ['companyA', 'companyA2', 'companyA3', 'companyD', 'companyD2', 'companyC', 'companyC2', 'companyB', 'companyB2', 'companyB3']
+
+
+############################################################## 
+import copy
+ 
+consultant_preferences = {
+ 'consultantE':  ['companyD', 'companyD2', 'companyC', 'companyC2', 'companyA', 'companyA2', 'companyB', 'companyB2'],
+ 'consultantH':  ['companyB', 'companyB2', 'companyC', 'companyC2', 'companyA', 'companyA2', 'companyD', 'companyD2'],
+ 'consultantF':  ['companyD', 'companyD2', 'companyA', 'companyA2', 'companyB', 'companyB2', 'companyC', 'companyC2'],
+ 'consultantA':  ['companyD', 'companyD2', 'companyB', 'companyB2', 'companyA', 'companyA2', 'companyC', 'companyC2'],
+ 'consultantG':  ['companyD', 'companyD2', 'companyC', 'companyC2', 'companyA', 'companyA2', 'companyB', 'companyB2'],
+ 'consultantD':  ['companyD', 'companyD2', 'companyB', 'companyB2', 'companyA', 'companyA2', 'companyC', 'companyC2'],
+ 'consultantC':  ['companyD', 'companyD2', 'companyA', 'companyA2', 'companyB', 'companyB2', 'companyC', 'companyC2'],
+ 'consultantB':  ['companyD', 'companyD2', 'companyA', 'companyA2', 'companyC', 'companyC2', 'companyB', 'companyB2']
+}
+project_preferences = {
+ 'companyA':  ['consultantA', 'consultantB', 'consultantC', 'consultantD', 'consultantE', 'consultantF', 'consultantG', 'consultantH'],
+ 'companyB':  ['consultantA', 'consultantC', 'consultantE', 'consultantD', 'consultantB', 'consultantG', 'consultantF', 'consultantH'],
+ 'companyC':  ['consultantA', 'consultantB', 'consultantE', 'consultantD', 'consultantC', 'consultantG', 'consultantH', 'consultantF'],
+ 'companyD':  ['consultantF', 'consultantE', 'consultantD', 'consultantA', 'consultantB', 'consultantC', 'consultantH', 'consultantG'],
+ 'companyA2':  ['consultantA', 'consultantB', 'consultantC', 'consultantD', 'consultantE', 'consultantF', 'consultantG', 'consultantH'],
+ 'companyB2':  ['consultantA', 'consultantC', 'consultantE', 'consultantD', 'consultantB', 'consultantG', 'consultantF', 'consultantH'],
+ 'companyC2':  ['consultantA', 'consultantB', 'consultantE', 'consultantD', 'consultantC', 'consultantG', 'consultantH', 'consultantF'],
+ 'companyD2':  ['consultantF', 'consultantE', 'consultantD', 'consultantA', 'consultantB', 'consultantC', 'consultantH', 'consultantG']
+}
+ 
+consultants = sorted(consultant_preferences.keys())
+projects = sorted(project_preferences.keys())
+ 
+ 
+def check(matched):
+    inversematched = dict((v,k) for k,v in matched.items())
+    for project, consultant in matched.items():
+        projectlikes = project_preferences[project]
+        projectlikesbetter = projectlikes[:projectlikes.index(consultant)]
+        consultantlikes = consultant_preferences[consultant]
+        consultantlikesbetter = consultantlikes[:consultantlikes.index(project)]
+        for teammate in projectlikesbetter:
+            teammatesgroup = inversematched[teammate]
+            teammatelikes = consultant_preferences[teammate]
+            if teammatelikes.index(teammatesgroup) > teammatelikes.index(project):
+                print("%s and %s like each other better than "
+                      "their present partners: %s and %s, respectively"
+                      % (project, teammate, consultant, teammatesgroup))
+                return False
+        for group in consultantlikesbetter:
+            groupsteammate = matched[group]
+            grouplikes = project_preferences[group]
+            if grouplikes.index(groupsteammate) > grouplikes.index(consultant):
+                print("%s and %s like each other better than "
+                      "their present partners: %s and %s, respectively"
+                      % (consultant, group, project, groupsteammate))
+                return False
+    return True
+ 
+def matchmaker():
+    consultantsfree = consultants[:]
+    matched  = {}
+    consultant_preferences2 = copy.deepcopy(consultant_preferences)
+    project_preferences2 = copy.deepcopy(project_preferences)
+    while consultantsfree:
+        consultant = consultantsfree.pop(0)
+        consultantslist = consultant_preferences2[consultant]
+        project = consultantslist.pop(0)
+        match = matched.get(project)
+        if not match:
+            # the project's free
+            matched[project] = consultant
+            print("  %s chooses %s" % (project, consultant))
+        else:
+            # The consultant proposes to an matched project!
+            projectslist = project_preferences2[project]
+            if projectslist.index(match) > projectslist.index(consultant):
+                # project prefers new consultant
+                matched[project] = consultant
+                print("  %s prefers %s over %s" % (project, consultant, match))
+                if consultant_preferences2[match]:
+                    # Ex has more projects to try
+                    consultantsfree.append(match)
+            else:
+                # project is faithful to old match
+                if consultantslist:
+                    # Look again
+                    consultantsfree.append(consultant)
+    return matched
+ 
+ 
+print('\nMatches:')
+matched = matchmaker()
+ 
+print('\nFinal Selections:')
+print('  ' + ',\n  '.join('%s is matched to %s' % couple
+                          for couple in sorted(matched.items())))
+print()
+print('Match stability check PASSED'
+      if check(matched) else 'Match stability check FAILED')
+ 
+print('\n\nSwapping two matches to introduce an error')
+matched[projects[0]], matched[projects[1]] = matched[projects[1]], matched[projects[0]]
+for project in projects[:2]:
+    print('  %s is now matched to %s' % (project, matched[project]))
+print()
+print('Match stability check PASSED'
+      if check(matched) else 'Match stability check FAILED')
+
+
+
+
+
+
+
+
